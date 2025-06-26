4fcf5a5b6163770796db87cf3a828201bf4efc4e
HHaii <2412nguyenhoanghai@gmail.com>
2025-06-26 21:06:32 +0700

    update

---
+++ lib/_selection.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# ╦╔╦╗╔═╗╔═╗╦═╗╔╦╗╔═╗
+# ║║║║╠═╝║ ║╠╦╝ ║ ╚═╗
+# ╩╩ ╩╩  ╚═╝╩╚═ ╩ ╚═╝ IMPORTS
+#==================================================
+# Regular + Autodesk
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.UI import *
+
+# .NET Imports
+import clr
+clr.AddReference("System")
+from System.Collections.Generic import List
+
+# ╦  ╦╔═╗╦═╗╦╔═╗╔╗ ╦  ╔═╗╔═╗
+# ╚╗╔╝╠═╣╠╦╝║╠═╣╠╩╗║  ║╣ ╚═╗
+#  ╚╝ ╩ ╩╩╚═╩╩ ╩╚═╝╩═╝╚═╝╚═╝ VARIABLES
+#==================================================
+uidoc = __revit__.ActiveUIDocument
+doc   = __revit__.ActiveUIDocument.Document #type: Document
+selection = uidoc.Selection #type: Selection
+
+# ╔═╗╦ ╦╔╗╔╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+# ╠╣ ║ ║║║║║   ║ ║║ ║║║║╚═╗
+# ╚  ╚═╝╝╚╝╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+#==================================================
